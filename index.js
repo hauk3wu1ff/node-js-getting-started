5a767e3a043e330b889864080a5c5ccae6e7288c
hauk3wu1ff <hauk3wu1ff@gmail.com>
2017-07-27 09:00:17 +0100

    Added  times function to demo config vars

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ app.get('/cool', function(request, response){
   response.send(cool());
 });
 
+app.get('/times', function(request, response){
+  var result = '';
+  var times = process.env.TIMES || 5;
+  for (var index = 0; index < times; index++) {
+    result += index.toString() + ' '; 
+  }
+  response.send(result);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
